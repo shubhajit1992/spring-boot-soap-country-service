82289a18acb56e9bc1200191da610cef41e60072
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2022-11-19 13:18:59 +0530

    Initial commit

---
+++ src/main/java/com/shubhajit/springbootsoapcountryservice/SpringBootSoapCountryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shubhajit.springbootsoapcountryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapCountryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapCountryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhajit/springbootsoapcountryservice/SpringBootSoapCountryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhajit.springbootsoapcountryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapCountryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
